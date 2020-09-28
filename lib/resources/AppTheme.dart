70564a4e70a31ed7146437324661e5a8dc026038
HydraLoD <alejandro.rdz10@gmail.com>
2020-09-27 20:08:19 -0600

    se corrigen tipos de datos de colores a final

--- lib/resources/AppTheme.dart
+++ lib/resources/AppTheme.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static Color primaryColor = new Color(0xffb71c1c);
-  static Color primaryLightColor = new Color(0xfff05545);
-  static Color primaryDarkColor = new Color(0xff7f0000);
-  static Color secondaryColor = new Color(0xff757575);
-  static Color secondaryLightColor = new Color(0xffa4a4a4);
-  static Color secondaryDarkColor = new Color(0xff494949);
-  static Color primaryTextColor = new Color(0xffffffff);
-  static Color secondaryTextColor = new Color(0xfffcfcfc);
+  static final Color primaryColor = new Color(0xffb71c1c);
+  static final Color primaryLightColor = new Color(0xfff05545);
+  static final Color primaryDarkColor = new Color(0xff7f0000);
+  static final Color secondaryColor = new Color(0xff757575);
+  static final Color secondaryLightColor = new Color(0xffa4a4a4);
+  static final Color secondaryDarkColor = new Color(0xff494949);
+  static final Color primaryTextColor = new Color(0xffffffff);
+  static final Color secondaryTextColor = new Color(0xfffcfcfc);
 }
