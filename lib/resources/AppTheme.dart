f85a01038e6d6d78dcddb0e97962f59a8bdc4711
HydraLoD <alejandro.rdz10@gmail.com>
2020-09-27 18:28:19 -0600

    Se corrigió error en archivo de colores

--- lib/resources/AppTheme.dart
+++ lib/resources/AppTheme.dart
@@ -5,7 +5,7 @@ class Apptheme {
   static Color primaryLightColor = new Color(0xfff05545);
   static Color primaryDarkColor = new Color(0xff7f0000);
   static Color secondaryColor = new Color(0xff757575);
-  static Color secondaryLightColor = new Color(0xffa4a4a4;
+  static Color secondaryLightColor = new Color(0xffa4a4a4);
   static Color secondaryDarkColor = new Color(0xff494949);
   static Color primaryTextColor = new Color(0xffffffff);
   static Color secondaryTextColor = new Color(0xfffcfcfc);
