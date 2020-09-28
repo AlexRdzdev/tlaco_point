6bccdeaea91b7c3afbeb8fbf2bfcefe2e17e3548
DianaNMkp <nolascomdiana@gmail.com>
2020-09-27 20:12:21 -0600

    log in

--- lib/src/pages/log_in/access_type.dart
+++ lib/src/pages/log_in/access_type.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
+
 import 'package:tlaco_point/resources/AppTheme.dart';
 
 class accessType extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[           
-          RaisedButton(
-            onPressed: () {},
-            child: const Text('Log In', style: TextStyle(fontSize: 20)),
-            color: ,
-          ),
-        ]
-        ),
-        
-      )
-
-
-    );
+        body: Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {},
+              child: const Text('Log In', style: TextStyle(fontSize: 20)),
+            ),
+          ]),
+    ));
   }
 }
