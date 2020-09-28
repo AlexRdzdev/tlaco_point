3b39e688a37505f820cf108e46e5fd00b186c70b
DianaNMkp <nolascomdiana@gmail.com>
2020-09-27 20:44:19 -0600

    Prototipo de pantalla log in

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tlaco_point/src/pages/home_page.dart';
+import 'package:tlaco_point/src/pages/log_in/access_type.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
-    return MaterialApp(
-        home: Center(
-      child: HomePage(),
-    ));
+    return MaterialApp(home: Center(child: AccessType()));
   }
 }
--- lib/src/pages/log_in/access_type.dart
+++ lib/src/pages/log_in/access_type.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
-
 import 'package:tlaco_point/resources/AppTheme.dart';
 
-class accessType extends StatelessWidget {
+class AccessType extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {},
-              child: const Text('Log In', style: TextStyle(fontSize: 20)),
-            ),
-          ]),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+          Widget>[
+        RaisedButton(
+          onPressed: () {},
+          child: Text('Log In',
+              style: TextStyle(fontSize: 20, color: AppTheme.primaryTextColor)),
+        ),
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () {},
+          child: Text('Acceder',
+              style: TextStyle(fontSize: 20, color: AppTheme.primaryTextColor)),
+        ),
+      ]),
     ));
   }
 }
