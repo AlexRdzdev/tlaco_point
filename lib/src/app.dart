58013a3e7422104604b718cac23fea852ab61afe
HydraLoD <alejandro.rdz10@gmail.com>
2020-09-27 18:05:41 -0600

    se crean pantallas de prueba

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tlaco_point/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+        home: Center(
+      child: HomePage(),
+    ));
+  }
+}
