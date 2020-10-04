cf10483364a35d15423bef05ad7e1b56640d1af6
HydraLoD <alejandro.rdz10@gmail.com>
2020-10-03 18:38:58 -0600

    .

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tlaco_point/resources/AppTheme.dart';
+import 'package:tlaco_point/src/pages/log_in/log_inn.dart';
 import 'package:tlaco_point/src/pages/main_menu.dart';
 import 'package:tlaco_point/src/pages/log_in/loading.dart';
 
@@ -7,15 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
-      home: Center(child: Loading()),
-      theme: ThemeData(
-          primaryColor: AppTheme.primaryColor,
-          primaryColorLight: AppTheme.primaryLightColor,
-          primaryColorDark: AppTheme.primaryDarkColor,
-          backgroundColor: AppTheme.white,
-          buttonTheme: ButtonThemeData(
-              buttonColor: AppTheme.primaryLightColor,
-              splashColor: AppTheme.primaryDarkColor)),
-    );
+        home: Center(child: Loginn()),
+        theme: ThemeData(
+            primaryColor: AppTheme.primaryColor,
+            primaryColorLight: AppTheme.primaryLightColor,
+            primaryColorDark: AppTheme.primaryDarkColor,
+            backgroundColor: AppTheme.white,
+            buttonTheme: ButtonThemeData(
+                buttonColor: AppTheme.primaryLightColor,
+                splashColor: AppTheme.primaryDarkColor,
+                textTheme: ButtonTextTheme.primary)));
   }
 }
