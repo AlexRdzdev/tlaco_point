f2fa420cf653bfe05d7d4c17cb2bac8c5c9b22d0
HydraLoD <alejandro.rdz10@gmail.com>
2020-10-03 18:03:30 -0600

    Se prepara pantalla de registro de puestos.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:tlaco_point/src/pages/home_page.dart';
+import 'package:tlaco_point/resources/AppTheme.dart';
 import 'package:tlaco_point/src/pages/log_in/access_type.dart';
+import 'package:tlaco_point/src/pages/main_menu.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
-    return MaterialApp(home: Center(child: AccessType()));
+    return MaterialApp(
+      home: Center(child: MainMenu()),
+      theme: ThemeData(
+          primaryColor: AppTheme.primaryColor,
+          primaryColorLight: AppTheme.primaryLightColor,
+          primaryColorDark: AppTheme.primaryDarkColor),
+    );
   }
 }
---
+++ lib/src/pages/log_in/Sign_up_stand.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUpStand extends StatefulWidget {
+  _SignUpStandState createState() => _SignUpStandState();
+}
+
+class _SignUpStandState extends State<SignUpStand> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
