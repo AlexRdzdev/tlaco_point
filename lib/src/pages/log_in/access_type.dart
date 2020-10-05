b8c43268110cfa873d98d8a962507abdcd0bf3e0
DianaNMkp <nolascomdiana@gmail.com>
2020-10-04 19:36:37 -0600

    Cambios de Icono, y nueva ventana de Loading

--- lib/src/pages/log_in/access_type.dart
+++ lib/src/pages/log_in/access_type.dart
@@ -5,23 +5,41 @@ class AccessType extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/TlacoPoint.jpg'),
-            SizedBox(height: 30),
-            RaisedButton(
-                onPressed: () {},
-                child: Text('Acceder', style: TextStyle(fontSize: 20))),
-            const SizedBox(height: 30),
-            RaisedButton(
-                onPressed: () {},
-                child: Text('Crear cuenta ', style: TextStyle(fontSize: 20))),
-            RaisedButton(
-                onPressed: () {},
-                child:
-                    Text('Registrar Puesto', style: TextStyle(fontSize: 20))),
-          ]),
+      child: Column(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Image.asset(
+            'assets/TlacoPoint.png',
+            width: 300.0,
+            height: 250.0,
+            alignment: Alignment.center,
+          ),
+        ),
+        const SizedBox(height: 30),
+        RaisedButton(
+            onPressed: () {},
+            child: Text(
+              'Acceder',
+              style: TextStyle(fontSize: 20),
+            )),
+        const SizedBox(height: 30),
+        RaisedButton(
+            onPressed: () {},
+            child: Text('Crear cuenta ', style: TextStyle(fontSize: 20))),
+        const SizedBox(height: 70),
+        const Text.rich(TextSpan(
+            text: 'Tienes un puesto? Registralo',
+            style: TextStyle(fontWeight: FontWeight.bold))),
+        const SizedBox(height: 20),
+        RaisedButton(
+            onPressed: () {},
+            child: Text('Registrar Puesto', style: TextStyle(fontSize: 20))),
+      ]),
     ));
   }
 }
+
+//Image.asset(
+//              'assets/TlacoPoint.png',
+//            scale: 10.0,
+//        ),
--- lib/src/pages/log_in/loading.dart
+++ lib/src/pages/log_in/loading.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:tlaco_point/resources/AppColors.dart';
+import 'package:tlaco_point/src/pages/Theme/AppTheme.dart';
+import 'access_type.dart';
 
 class Loading extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Center(child: Image.asset('assets/TlacoPoint.png')),
-      ),
+  Widget build(context) {
+    return MaterialApp(
+      home: Splash2(),
     );
+  }
+}
+
+class Splash2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 3,
+        navigateAfterSeconds: new SecondScreen(),
+        image: new Image.asset('assets/TlacoPoint.png'),
+        loadingText: Text("Loading"),
+        photoSize: 100.0,
+        loaderColor: AppColors.primaryDarkColor);
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(child: AccessType()),
+        theme: AppTheme.getAppTheme(context));
   }
 }
