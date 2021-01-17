037a011844fcc387911ca3db57b08b4d90fcbf1e
DianaNMkp <nolascomdiana@gmail.com>
2021-01-17 00:04:03 -0700

    Estos son cambios para agregar las categorias que tendra la app (descartar, aun no esta terminado )

--- lib/src/pages/home_pages/start_page.dart
+++ lib/src/pages/home_pages/start_page.dart
@@ -1,8 +1,74 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class StartFirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('First Page'));
+    return Scaffold(
+        appBar:
+            AppBar(leading: Icon(Icons.category), title: Text("Categorias")),
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                children: <Widget>[_botonesRedondeados()],
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget _botonesRedondeados() {
+    return Table(
+      children: [
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.blue, Icons.directions_bus,
+              'Pizza'), //Image.asset('assets/pizza.png')
+          _crearBotonRedondeado(
+              Colors.purpleAccent, Icons.directions_bus, 'Bus'),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+          _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File'),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(
+              Colors.blueAccent, Icons.movie_filter, 'Entertaiment'),
+          _crearBotonRedondeado(Colors.green, Icons.cloud, 'Grocery'),
+        ]),
+        TableRow(children: [
+          _crearBotonRedondeado(Colors.red, Icons.collections, 'Photos'),
+          _crearBotonRedondeado(Colors.teal, Icons.help_outline, 'General'),
+        ])
+      ],
+    );
+  }
+
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20.0)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icono, color: Colors.white, size: 30.0),
+              ),
+              Text(texto, style: TextStyle(color: color)),
+              SizedBox(height: 5.0)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
