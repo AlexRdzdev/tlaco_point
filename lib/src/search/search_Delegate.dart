b00f66dd1f98276ae5c06cc176d25d8682a1f584
HydraLoD <alejandro.rdz10@gmail.com>
2021-01-17 14:25:42 -0700

    Barra de busquedas por nombre del puesto

---
+++ lib/src/search/search_Delegate.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:tlaco_point/models/puesto.dart';
+import 'package:tlaco_point/services/Stands/searchStandService.dart';
+
+class StandSearch extends SearchDelegate {
+  StandSearch({
+    String hintText,
+  }) : super(
+          searchFieldLabel: hintText,
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.search,
+        );
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro AppBar
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del AppBar
+    return IconButton(
+        icon: AnimatedIcon(
+          progress: transitionAnimation,
+          icon: AnimatedIcons.menu_arrow,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que vamos a mostrar
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Las sugerencias que aparecen cuando la persona escribe
+    if (query.isEmpty) return Container();
+    return FutureBuilder(
+      future: SearchStandService.buscar(pNOMBRE: query),
+      builder: (BuildContext context, AsyncSnapshot<List<Puesto>> snapshot) {
+        if (snapshot.hasData) {
+          final puestos = snapshot.data;
+          return ListView(
+            children: puestos.map((puesto) {
+              return ListTile(
+                leading: Icon(Icons.store),
+                title: Text(puesto.tpFranquicia.nombreFranquicia),
+                subtitle: Text(puesto.tpFranquicia.especialidad),
+                onTap: () {},
+              );
+            }).toList(),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
